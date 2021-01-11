63cb2f797ccc9b3fb7cbafa0eb866d95b881fccf
technorbi <takacs.norbert1205@gmail.com>
2021-01-11 18:09:32 +0100

    feat: menu, ship choose

--- game.js
+++ game.js
@@ -2,14 +2,65 @@ initGame();
 
 function initGame() {
 
-    const singlePlayer = document.querySelector('.single')
+    const singlePlayerButton = document.querySelector('.single');
+    const highScoreButton = document.querySelector('.high-score');
+    const creditsButton = document.querySelector('.credits');
+    const logoContainer = document.querySelector('.logo-container');
 
-    function newGame(){
-        singlePlayer.addEventListener('click', function(){
-            newGame()
-        })
-        console.log("newGame")
+    singlePlayerButton.addEventListener("click", startGame)
+
+    function drawShip() {
+
     }
+
+    function startGame(){
+
+        // Hide Menu
+        if (logoContainer.style.display === "none") {
+            logoContainer.style.display = "block";
+            }
+        else {
+            logoContainer.style.display = "none";
+            }
+        chooseShip()
+
+    }
+
+    function chooseShip(){
+
+        // Display
+        const shipChoose = document.querySelector('.ship-choose')
+        shipChoose.style.display = 'block'
+
+        // Select ships
+        const xWing = document.querySelector('.xwing')
+        const slaveOne = document.querySelector('.slave')
+        const milleniumFalcon = document.querySelector('.falcon')
+
+        // Eventlisteners
+        let chosenShip = ''
+        xWing.addEventListener('click', chooseShip)
+        slaveOne.addEventListener('click', chooseShip)
+        milleniumFalcon.addEventListener('click', chooseShip)
+
+        console.log(chosenShip)
+
+        // Ship choose
+        function chooseShip() {
+            chosenShip = // TODO ship id!!!
+
+            // Hide container
+            shipChoose.style.display = 'none'
+
+            return chosenShip
+        }
+
+
+
+
+
+    }
+
     function showCredits(){
 
     }
